cd376dfd4fa97a881ba5a03f233c021ca1cd7b29
hichtoor <hichtoor@gmail.com>
2026-07-30 10:12:47 +0330

    add urls for main pages and connect navigation links

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 from core.views import *
 
+app_name = 'core'
+
 urlpatterns = [
     path('', index, name='index'),
+    path('about', about, name='about'),
+    path('contact', contact, name='contact'),
+
+
 ]
