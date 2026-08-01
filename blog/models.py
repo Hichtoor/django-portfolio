a6a1af7d1039165850d4eadf7f458912bde5a44f
hichtoor <hichtoor@gmail.com>
2026-08-01 09:43:15 +0330

    add initial blog post model and admin registration

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    status = models.BooleanField(default=False)
+    counted_views = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    published_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now=True)
