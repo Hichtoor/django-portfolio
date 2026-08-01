192f3b8013e08882a4a2ad6d5a7431424f700420
hichtoor <hichtoor@gmail.com>
2026-08-01 10:16:36 +0330

    display posts on blog home page

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 
+from blog.models import Post
+
+
 # Create your views here.
 
 def home(request):
-    return render(request, 'blog/home.html')
+    posts = Post.objects.filter(status=1)
+    context = {'posts': posts}
+    return render(request, 'blog/home.html', context)
