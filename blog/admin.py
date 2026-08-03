53487ffd71b2733082511b75fbb93a317b6b3d64
hichtoor <hichtoor@gmail.com>
2026-08-03 08:57:56 +0330

    customize Django admin configuration

--- blog/admin.py
+++ blog/admin.py
@@ -7,4 +7,8 @@ from blog.models import Post
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    pass
+    date_hierarchy = 'published_at'
+    list_display = ('title', 'content', 'status', 'published_at')
+    list_filter = ('status',)
+    search_fields = ('title', 'content')
+    empty_value_display = '-empty-'
