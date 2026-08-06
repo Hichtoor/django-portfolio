ba322fd0d245536db028203372f3791d5b255d76
hichtoor <hichtoor@gmail.com>
2026-08-06 09:15:25 +0330

    add image support for blog posts

--- blog/models.py
+++ blog/models.py
@@ -7,6 +7,7 @@ class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    image = models.ImageField(upload_to='blog/', default='blog/default.jpg')
     status = models.BooleanField(default=False)
     counted_views = models.IntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
