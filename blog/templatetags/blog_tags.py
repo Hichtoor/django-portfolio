9df29a121dcb026b26aac60ff728f74a3e0d8bde
hichtoor <hichtoor@gmail.com>
2026-08-09 13:37:40 +0330

    add latest posts inclusion tag

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -6,3 +6,8 @@ register = template.Library()
 @register.filter
 def snippet(value, arg=100):
     return value[:arg] + ' ...'
+
+@register.inclusion_tag('blog/latest-posts.html')
+def latest_posts(arg=3):
+    posts = Post.objects.filter(status=True).order_by('-published_at')[:arg]
+    return {'posts': posts}
