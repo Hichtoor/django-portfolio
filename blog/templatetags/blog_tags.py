920d3664f7e29dc63bbacef6959b4b450203faa1
hichtoor <hichtoor@gmail.com>
2026-08-10 10:30:52 +0330

    display post categories in sidebar using inclusion tag

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,5 +1,5 @@
 from django import template
-from blog.models import Post
+from blog.models import Post, Category
 
 register = template.Library()
 
@@ -11,3 +11,13 @@ def snippet(value, arg=100):
 def latest_posts(arg=3):
     posts = Post.objects.filter(status=True).order_by('-published_at')[:arg]
     return {'posts': posts}
+
+@register.inclusion_tag('blog/post-categories.html')
+def post_categories():
+        posts = Post.objects.filter(status=True)
+        categories = Category.objects.all()
+        cat_dict = {}
+        for name in categories:
+            cat_dict[name] = posts.filter(category=name).count()
+
+        return {'categories': cat_dict}
