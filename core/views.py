158ae728b2cab7f4fca24ea8fa143f1decf8cd60
hichtoor <hichtoor@gmail.com>
2026-08-18 10:09:49 +0330

    implement contact form submission

--- core/views.py
+++ core/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
+from core.forms import ContactForm
+
+
 # Create your views here.
 
 def index(request):
@@ -9,4 +12,10 @@ def about(request):
     return render(request, 'about.html')
 
 def contact(request):
-    return render(request, 'contact.html')
+
+    if request.method == "POST":
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+    form = ContactForm()
+    return render(request, 'contact.html', {'form': form})
